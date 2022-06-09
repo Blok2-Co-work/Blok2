0d894fcf2d546b2e98f82a726472fbac41b9cbce
AvkSarEZ <96582917+AvkSarEZ@users.noreply.github.com>
2022-06-10 00:58:26 +0200

    Add files via upload

--- gitgame_backend_git/router/controller/controller.js
+++ gitgame_backend_git/router/controller/controller.js
@@ -51,7 +51,7 @@ exports.setScore = (req, res) => {
     connection.connect();
 
     connection.query(`
-    INSERT INTO scores ('GitGame', '${req.body.username}', '${req.body.score}', 2, NOW());
+    INSERT INTO scores (game, user, score, difficulty, date) VALUES ('GitGame', '${req.body.username}', '${req.body.score}', 2, NOW());
     `, (err, rows) => {
         if (err){
             res.status(500).json({status: err});
