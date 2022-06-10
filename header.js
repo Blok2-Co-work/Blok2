c7d438eeb22af60699dd57b44b9290faeff7e055
AvkSarEZ <96582917+AvkSarEZ@users.noreply.github.com>
2022-06-10 10:54:47 +0200

    Delete header.js

--- header.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('#menu');
-
-button.addEventListener('click', function () {
-    this.setAttribute('aria-expanded', this.getAttribute('aria-expanded') === "true" ? "false" : "true")
-
-})
