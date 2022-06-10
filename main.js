fd2401baae8c8854f2a6b0ee86d3a0e75ea69359
JasperBrands <104193194+JasperBrands@users.noreply.github.com>
2022-06-10 10:26:20 +0200

    final version

---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelector(".play2048").addEventListener("click",function(){
+    document.location.href = "./2048-2.0/2048.html";
+});
+
+document.querySelector(".play2048yes").addEventListener("click",function(){
+    document.location.href = "./2048-2.0/2048.html";
+});
+document.querySelector(".leaderboardButton").addEventListener("click",function(){
+    document.location.href = "./contact_and_leaderboards/leaderboard/" 
+})
+document.querySelector(".contactButton").addEventListener("click",function(){
+    document.location.href = "./contact_and_leaderboards/contact/"
+})
